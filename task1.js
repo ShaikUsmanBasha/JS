00ac01f9f0d09a896a9f7fae05685d8a1f628881
ShaikUsmanBasha <usmanbashashaik524@gmail.com>
2026-06-04 18:52:04 +0530

    Java Script Tasks

---
+++ task1.js
@@ -0,0 +1,74 @@
+// Task1
+let StudentName = "Usman";
+let Studentage = 24;
+let StudentCourse = "Full Stack"
+
+console.log(StudentName);
+console.log(Studentage);
+console.log(StudentCourse);
+
+// Task2
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+
+company = "Google";
+salary = 30000;
+
+console.log(company);
+console.log(salary);
+console.log(country);
+
+// Task3
+let Name = prompt("what is your name:");
+console.log("Welcome" + Name);
+
+// Task4
+let result = confirm("Do you know Javascript?");
+
+console.log(result);
+
+// Task5
+let studentName = prompt("Enter the name:");
+let studentAge = prompt("Enter the age:");
+let studentcity = prompt("Enter the city");
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentcity);
+
+// Task6
+let course = "Java Script";
+alert("Welcome" + course);
+
+// Task7
+
+document.writeln("Hello Student"); 
+document.writeln("<br>");
+document.writeln("Welcome to JavaScript");
+
+// Task8
+let FavoriteFood = "chicken biriyani";
+let FavoriteColor = "Red";
+let FavoriteMovie = "Pushpa";
+
+console.log("Food:" +FavoriteFood);
+console.log("Color:" +FavoriteColor);
+console.log("Movie:" +FavoriteMovie);
+
+// Task9
+let Name1 = "Shaik Usman Basha";
+let age = 24;
+let phone = "8142652711";
+let email = "usman@gmail.com";
+
+
+console.log("Name:" + Name1);
+console.log("Age:" + age);
+console.log("Phone:" + phone);
+console.log("email:" + email);
+
+// Task10
+let name1 = prompt("enter the name");
+let course2 = prompt("enter the course");
+alert("Hello" + name1 +"Welcome to"+ course2)
